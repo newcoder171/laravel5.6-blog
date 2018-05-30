dab623d0dcc2316eaf304af57369d23ad0e8ad7f
newcoder <newcoder17@gmail.com>
2018-05-31 00:07:35 +0100

    add auth

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,11 +11,19 @@
         <div class="bg-info text-white p-3 m-3">
             <a href="{{route('posts.index')}}" class="btn btn-primary">Home</a>
             <a href="{{route('posts.create')}}" class="btn btn-primary">Create Post</a>
+            @auth
+            <form class="d-inline-block float-right" action="{{route('logout')}}" method="post">
+            @csrf
+            <button class="btn btn-primary">{{auth()->user()->name}} | Logout</button>
+            </form>
+            @else
+                <a href="{{route('login')}}" class="btn btn-primary float-right">Login</a>
+            @endauth
         </div>
         <div class="container">
             @yield('content')
         </div>
-        <div class="bg-dark text-white p-4">
+        <div class="bg-dark text-white p-4 text-center">
             All rights reserved
         </div>
     </body>
